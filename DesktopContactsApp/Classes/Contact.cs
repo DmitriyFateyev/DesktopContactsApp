0195fe6bebc424ba655816604d6ed9e1f58bf00a
DmitriyFate <ds009@DNOTEBOOK>
2021-11-18 17:06:07 +0400

    Amen

--- DesktopContactsApp/Classes/Contact.cs
+++ DesktopContactsApp/Classes/Contact.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SQLite;
 
 namespace DesktopContactsApp.Classes
 {
-    class Contact
+    public class Contact
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
