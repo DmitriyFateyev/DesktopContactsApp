7d4d0e54b3bce2aed16b9ab16fa7738469cca89d
DmitriyFate <ds009@DNOTEBOOK>
2021-11-19 16:27:39 +0400

    DB connection

--- DesktopContactsApp/NewContactWindow.xaml.cs
+++ DesktopContactsApp/NewContactWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
+using System.Windows;
 
 namespace DesktopContactsApp
 {
@@ -12,9 +15,25 @@ namespace DesktopContactsApp
             InitializeComponent();
         }
 
-        private void saveButton_Click(object sender, RoutedEventArgs e)
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            // Save contact
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneTextBox.Text
+            };
+
+            string databaseName = "Contacts.db";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string databasePath = System.IO.Path.Combine(folderPath + databaseName);
+
+            using (SQLiteConnection dbConnection = new SQLiteConnection(databasePath))
+            {
+                dbConnection.CreateTable<Contact>();
+                dbConnection.Insert(contact);
+            }
+
             Close();
         }
     }
